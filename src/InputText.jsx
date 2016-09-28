de3125db76be50e58bfb677af0a51cfb543b603a
Daniela Borges <alunassertiva@gmail.com>
2016-09-28 17:02:17 +0100

    InputText

---
+++ src/InputText.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classNames from 'classnames';
+
+const InputText = (props) => {
+  const { field, label, type, fieldName, placeholder, validationErrors } = props;
+  const classes = classNames({
+    'form-group': true,
+    'has-error': field.touched && validationErrors && validationErrors.length
+  });
+
+  return (
+    <div className={classes}>
+      <label htmlFor={fieldName} className="control-label col-xs-3">{label}</label>
+      <div className="col-xs-9">
+        <input className="form-control" id={fieldName} type={type} placeholder={placeholder} {...field} />
+        { field.touched && validationErrors && validationErrors.length && <div className="help-block">{ validationErrors[0] }</div> }
+      </div>
+    </div>
+  );
+};
+
+
+InputText.propTypes = {
+  field: React.PropTypes.object.isRequired,
+  label: React.PropTypes.string.isRequired,
+  type: React.PropTypes.string.isRequired,
+  fieldName: React.PropTypes.string.isRequired,
+  placeholder: React.PropTypes.string,
+  validationErrors: React.PropTypes.array
+};
+
+export default InputText;
