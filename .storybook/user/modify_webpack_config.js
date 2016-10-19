40bb2dbacbcc08b04aa127c27c58933b862d417f
Sandrino Di Mattia <contact@sandrino.be>
2016-10-19 11:32:19 +0200

    Only create a library when we build the module

--- .storybook/user/modify_webpack_config.js
+++ .storybook/user/modify_webpack_config.js
@@ -1,34 +1,37 @@
 const path = require('path');
 
 module.exports = function (config) {
-  // This is the default webpack config defined in the `../webpack.config.js`
-  // modify as you need.
+	// This is the default webpack config defined in the `../webpack.config.js`
+	// modify as you need.
 
-  config.module.loaders.push({
-    test: /\.styl$/,
-    loader: 'style-loader!css-loader!stylus-loader'
-  });
-  config.module.loaders.push({
-    test: /\.(png|ttf|svg|jpg|gif)/,
-    loader: 'url?limit=8192'
-  });
-  config.module.loaders.push({
-    test: /\.(woff|woff2|eot)/,
-    loader: 'url?limit=100000'
-  });
-  config.module.loaders.push({
-    test: /\.jsx?$/,
-    loader: 'babel',
-    exclude: path.join(__dirname, '../../node_modules/')
-  });
-  config.target = 'node';
-  config.entry = path.join(__dirname, '../../src/components/index.js');
-  config.output = {
-    path: './dist',
-    filename: 'index.js',
-    library: true,
-    libraryTarget: 'commonjs2'
-  };
+	config.module.loaders.push({
+		test: /\.styl$/,
+		loader: 'style-loader!css-loader!stylus-loader'
+	});
+	config.module.loaders.push({
+		test: /\.(png|ttf|svg|jpg|gif)/,
+		loader: 'url?limit=8192'
+	});
+	config.module.loaders.push({
+		test: /\.(woff|woff2|eot)/,
+		loader: 'url?limit=100000'
+	});
+	config.module.loaders.push({
+		test: /\.jsx?$/,
+		loader: 'babel',
+		exclude: path.join(__dirname, '../../node_modules/')
+	});
 
-  return config;
+	if (process.NODE_ENV === 'production') {
+		config.target = 'node';
+		config.entry = path.join(__dirname, '../../src/components/index.js');
+		config.output = {
+			path: './dist',
+			filename: 'index.js',
+			library: true,
+			libraryTarget: 'commonjs2'
+		};
+	}
+
+	return config;
 };
