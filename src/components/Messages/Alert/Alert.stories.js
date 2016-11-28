1dccc09d5da6d6281c3b3265016d9d46d86848f3
Daniela Borges <alunassertiva@gmail.com>
2016-11-28 16:30:42 +0000

    Alert stories

---
+++ src/components/Messages/Alert/Alert.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+
+import Alert from './';
+
+function renderField(field) {
+  return (
+    <Alert show={field.show} onDismiss={field.close} type={field.type} message={field.message} />
+  );
+}
+
+storiesOf('Alert', module)
+  .add('default view', () => {
+    const field = {
+      show: true,
+      type: 'info',
+      message: 'This is the alert message',
+      close: action('closeAlertMessage')
+    };
+    return renderField(field);
+  });
