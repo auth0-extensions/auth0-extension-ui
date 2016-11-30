f3939a14d1b93f9e549afbf0d95fba8af3504e35
Daniela Borges <alunassertiva@gmail.com>
2016-11-30 16:17:16 +0000

    checked value

--- src/components/Form/InputCheckBox/InputCheckBox.specs.js
+++ src/components/Form/InputCheckBox/InputCheckBox.specs.js
@@ -9,26 +9,31 @@ import InputCheckBox from './';
 const { describe, it } = global;
 
 describe('InputCheckBox', () => {
-  it('should render one input element', () => {
-    const wrapper = shallow(<InputCheckBox />);
-    expect(wrapper.find('input')).to.have.length(1);
-  });
-  it('should have input with type checkbox', () => {
-    const wrapper = shallow(<InputCheckBox />);
-    expect(wrapper.find('input').props().type).to.equal('checkbox');
-  });
-  it('should show the given value', () => {
-    const field = {
+  let field;
+  let wrapper;
+
+  beforeEach((done) => {
+    field = {
       name: 'FieldName',
       label: 'My Label'
     };
-    const wrapper = mount(addFormDecorator(
+    wrapper = mount(addFormDecorator(
       <Field
         name={field.name}
         component={InputCheckBox}
         label={field.label}
       />
     ));
+    done();
+  });
+
+  it('should render one input element', () => {
+    expect(wrapper.find('input')).to.have.length(1);
+  });
+  it('should have input with type checkbox', () => {
+    expect(wrapper.find('input').props().type).to.equal('checkbox');
+  });
+  it('should show the given value', () => {
     expect(wrapper.find('input').props().id).to.be.equal(field.name);
     expect(wrapper.find('label').text()).to.be.equal(field.label);
   });
