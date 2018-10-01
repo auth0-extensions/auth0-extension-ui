33847cae3c0d3830f8a3e111a98ac1688675d49d
Shawn Mclean <shawnmclean@users.noreply.github.com>
2018-10-01 14:11:06 +0100

    ZD-48075: Show default option on 1 or more options (#9)
    * Show default option when one or more  select option is passed in to InputCombo.
    
    * Update build.

--- src/components/Form/InputCombo/InputCombo.specs.js
+++ src/components/Form/InputCombo/InputCombo.specs.js
@@ -10,6 +10,30 @@ import InputCombo from './';
 const { describe, it } = global;
 
 describe('InputCombo', () => {
+  describe('when one or more options exist', () => {
+    it('should render an empty option', () => {
+      const field = {
+        name: 'FieldName',
+        label: 'My Label',
+        placeholder: 'My placeholder',
+        options: [ { value: 1, text: 'Option 1' } ]
+      };
+  
+      const wrapper = mount(addFormDecorator(
+        <Field
+          name={field.name}
+          component={InputCombo}
+          label={field.label}
+          placeholder={field.placeholder}
+          options={field.options}
+        />
+      ));
+      expect(wrapper.find('select').props().id).to.be.equal(field.name);
+      expect(wrapper.find('label').text()).to.be.equal(field.label);
+      expect(wrapper.find('option')).to.have.length(2); // option plus default message
+    });
+  });
+
   it('should render one select element', () => {
     const field = {
       name: 'FieldName',
@@ -27,28 +51,6 @@ describe('InputCombo', () => {
       />
     ));
     expect(wrapper.find('select')).to.have.length(1);
-  });
-
-  it('should show the given values', () => {
-    const field = {
-      name: 'FieldName',
-      label: 'My Label',
-      placeholder: 'My placeholder',
-      options: [ { value: 1, text: 'Option 1' }, { value: 2, text: 'Option 2' } ]
-    };
-
-    const wrapper = mount(addFormDecorator(
-      <Field
-        name={field.name}
-        component={InputCombo}
-        label={field.label}
-        placeholder={field.placeholder}
-        options={field.options}
-      />
-    ));
-    expect(wrapper.find('select').props().id).to.be.equal(field.name);
-    expect(wrapper.find('label').text()).to.be.equal(field.label);
-    expect(wrapper.find('option')).to.have.length(3); // two options plus default message
   });
 
   it('should call onChange func if exists', () => {
